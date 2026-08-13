8c7e24ef385ab6c7540bdcd930a13379d8741208
souzad12 <104470327+souzad12@users.noreply.github.com>
2026-08-13 16:34:06 -0300

    Route writer through v044 prompts

--- chs-android-v0.1/app/src/main/java/com/churchhistorystudio/app/WriterPrompts.java
+++ chs-android-v0.1/app/src/main/java/com/churchhistorystudio/app/WriterPrompts.java
@@ -1,6 +1,10 @@
 package com.churchhistorystudio.app;
 
 final class WriterPrompts {
-  static String system() { return WriterPromptsV041.system(); }
-  static String user(String payloadJson) throws Exception { return WriterPromptsV041.user(payloadJson); }
+  static String system() {
+    return WriterPromptsV044.system();
+  }
+  static String user(String payloadJson) throws Exception {
+    return WriterPromptsV044.user(payloadJson);
+  }
 }
